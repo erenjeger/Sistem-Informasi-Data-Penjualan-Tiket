4e4c4cfc1af59cdb436baddaacbc735aa4f03e6e
kurniawisudaaji <kurniawisudaaji0@gmail.com>
2017-05-01 23:25:11 +0700

    Update ControllerPenumpangLihatTiket.java

--- Sistem Informasi Data Penjualan Tiket/src/Controller/ControllerPenumpangLihatTiket.java
+++ Sistem Informasi Data Penjualan Tiket/src/Controller/ControllerPenumpangLihatTiket.java
@@ -1,14 +1,44 @@
-/*
+/*/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
 package Controller;
 
+import Model.AplikasiGUI;
+import View.PenumpangLihatTiket;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  *
- * @author USER
+ * @author KURNIA
  */
-public class ControllerPenumpangLihatTiket {
+public class ControllerPenumpangLihatTiket implements ActionListener {
+    
+    AplikasiGUI model;
+    PenumpangLihatTiket view;
+    
+    public ControllerPenumpangLihatTiket(AplikasiGUI model) {
+        this.model = model;
+        view = new PenumpangLihatTiket();
+        view.setVisible(true);
+        view.addListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        
+        if(source.equals(view.getBtnOk())) {
+            new ControllerHalamanPenumpang(model);
+            view.dispose();
+        } else if (source.equals(view.getBtnLogout())) {
+            new ControllerHome(model);
+            view.dispose();
+        }
+    }
+    
+    
     
 }
