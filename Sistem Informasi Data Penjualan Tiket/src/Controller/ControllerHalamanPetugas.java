5b14bb196519905ad6bac3b8c38d05430237e273
kurniawisudaaji <kurniawisudaaji0@gmail.com>
2017-05-01 23:22:08 +0700

    Update ControllerHalamanPetugas.java

--- Sistem Informasi Data Penjualan Tiket/src/Controller/ControllerHalamanPetugas.java
+++ Sistem Informasi Data Penjualan Tiket/src/Controller/ControllerHalamanPetugas.java
@@ -5,10 +5,53 @@
  */
 package Controller;
 
+import View.HalamanPetugas;
+import Model.AplikasiGUI;
+import Model.Petugas;
+import Model.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 /**
  *
- * @author USER
+ * @author KURNIA
  */
-public class ControllerHalamanPetugas {
+public class ControllerHalamanPetugas implements ActionListener {
+    AplikasiGUI model;
+    HalamanPetugas view;
+    Petugas pet;
     
+    public void ControllerHalamanPetugas(AplikasiGUI model) {
+        this.model = model;
+        view = new HalamanPetugas();
+        view.setVisible(true);
+        view.addListener(this);
+    }
+ 
+    
+   
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = e.getSource();
+        
+        if(source.equals(view.getBtnLihatPenumpang())){
+            ControllerPetugasLihatPenumpang plp = new ControllerPetugasLihatPenumpang(model);
+            view.dispose();
+        } else if(source.equals(view.getBtnLihatKereta())){
+            ControllerPetugasLihatKereta plk = new ControllerPetugasLihatKereta(model);
+            view.dispose();
+        } else if(source.equals(view.getBtnLihatRute())){
+            ControllerPetugasLihatRute plr = new ControllerPetugasLihatRute(model);
+            view.dispose();
+        } else if(source.equals(view.getbtnLihatStasiun())){
+            ControllerPetugasLihatStasiun pls = new ControllerPetugasLihatStasiun(model);
+            view.dispose();
+        } else if(source.equals(view.getbtnTambahKereta())){
+            ControllerPetugasTambahKereta ptk = new ControllerPetugasTambahKereta(model);
+            view.dispose();
+        } else if (source.equals(view.getbtnLogout())) {
+            ControllerHome h = new ControllerHome(model);
+            view.dispose();
+        }
+    }
 }
