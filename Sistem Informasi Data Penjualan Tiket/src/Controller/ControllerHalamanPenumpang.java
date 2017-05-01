17f39e0e0abc1432ba59b970c6fb4a4d0d21ca0b
kurniawisudaaji <kurniawisudaaji0@gmail.com>
2017-05-01 23:20:15 +0700

    Update ControllerHalamanPenumpang.java

--- Sistem Informasi Data Penjualan Tiket/src/Controller/ControllerHalamanPenumpang.java
+++ Sistem Informasi Data Penjualan Tiket/src/Controller/ControllerHalamanPenumpang.java
@@ -5,10 +5,44 @@
  */
 package Controller;
 
+import Model.AplikasiGUI;
+import Model.Penumpang;
+import View.HalamanPenumpang;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+
 /**
  *
- * @author USER
+ * @author KURNIA
  */
-public class ControllerHalamanPenumpang {
+public class ControllerHalamanPenumpang implements ActionListener {    
+    AplikasiGUI model;
+    HalamanPenumpang view;
+    Penumpang pen;
+    
+    public ControllerHalamanPenumpang(AplikasiGUI model) {
+        this.model = model;
+        view = new HalamanPenumpang();
+        view.setVisible(true);
+        view.addListener(this);
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        Object source = new Object();
+        
+        if(source.equals(view.getBtnPesanTiket())) {
+            new ControllerPenumpangPesanTiket();
+            view.dispose();
+        } else if (source.equals(view.getBtnLihatTiket())) {
+            new ControllerPenumpangLihatTiket();
+            view.dispose();
+        } else if (source.equals(view.getBtnKeluar())) {
+            new ControllerHome(model);
+            view.dispose();
+        }
+    }
+    
     
 }
